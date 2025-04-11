d2fa52eb8f4dd7cbe3b2307995b148e9701e8cee
Isabellfr <froehner.isabe-it24@it.dhbw-ravensburg.de>
2025-04-11 10:31:20 +0200

    Software_Semester2

---
+++ Software_Semester2/HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+// This is a simple C++ program that prints "Hello, World!" to the console.
+// It includes the iostream library for input and output operations.
+// The main function is the entry point of the program.     
